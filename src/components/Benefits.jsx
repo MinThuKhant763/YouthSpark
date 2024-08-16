d0db4a32fa5aa2b7b378e61184d4750c1735c0f2
Min Thu Khant <85979978+MinThuKhant763@users.noreply.github.com>
2024-08-16 11:48:06 +0630

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import BenefitsCard from './BenefitsCard';
-import TextScroll from './textScroll';
+
 const Benefits = () => {
     return (
         <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
@@ -10,7 +10,7 @@ const Benefits = () => {
                 <div className='text-xl py-8'>Small Modular Reactors (SMRs), are increasingly important for several reasons:
                 </div>
                 <BenefitsCard />
-                < TextScroll />
+               
             </div>
       </div>
    
